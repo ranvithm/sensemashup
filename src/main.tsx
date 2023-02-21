4c295618e045145fb205ccca3f0d8147b8bf38ef
Ranjithkumar M <ranvitranjit@gmail.com>
2023-02-21 20:25:30 +0530

    update build

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,8 @@ import {StrictMode} from 'react'
 import ReactDOM from 'react-dom/client'
 import {ThemeProvider} from '@mui/material'
 
-import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
-import '@fontsource/roboto/700.css'
 
 import App from './pages/App'
 import theme from './styles/theme'
--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,9 @@ export default defineConfig({
  server: {
   port: 3000,
  },
+ build: {
+  sourcemap: false,
+  rollupOptions: {
+  },
+ },
 })
